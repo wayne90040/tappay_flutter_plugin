f1609052e4102c0fa915b6a4e62ef639ee5a26f3
HSU WEI LUN <wayne90040@gmail.com>
2023-10-21 00:04:20 +0800

    refactor: refactor ios plugin

--- ios/Classes/SwiftTappayflutterpluginPlugin.swift
+++ ios/Classes/SwiftTappayflutterpluginPlugin.swift
@@ -2,9 +2,47 @@ import Flutter
 import UIKit
 import TPDirect
 import AdSupport
+import Foundation
+
+public enum Call {
+    case setupTappay
+    case isCardValid
+    case getPrime
+    case isEasyWalletAvailable
+    case getEasyWalletPrime
+    case redirectToEasyWallet
+    case isLinePayAvailable
+    case getLinePayPrime
+    case redirectToLinePay
+    case undefined
+
+    init?(_ call: String) {
+        switch call {
+        case "setupTappay":
+            self = .setupTappay
+        case "isCardValid":
+            self = .isCardValid
+        case "getPrime":
+            self = .getPrime
+        case "isEasyWalletAvailable":
+            self = .isEasyWalletAvailable
+        case "getEasyWalletPrime":
+            self = .getEasyWalletPrime
+        case "redirectToEasyWallet":
+            self = .redirectToEasyWallet
+        case "isLinePayAvailable":
+            self = .isLinePayAvailable
+        case "getLinePayPrime":
+            self = .getLinePayPrime
+        case "redirectToLinePay":
+            self = .redirectToLinePay
+        default:
+            self = .undefined
+        }
+    }
+}
 
 public class SwiftTappayflutterpluginPlugin: NSObject, FlutterPlugin {
-    
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "tappayflutterplugin", binaryMessenger: registrar.messenger())
         let instance = SwiftTappayflutterpluginPlugin()
@@ -12,19 +50,14 @@ public class SwiftTappayflutterpluginPlugin: NSObject, FlutterPlugin {
         registrar.addApplicationDelegate(instance)
     }
     
-    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable : Any] = [:]) -> Bool {
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable: Any] = [:]) -> Bool {
         TPDLinePay.addExceptionObserver(#selector(tappayLinePayExceptionHandler(notofication:)))
         TPDEasyWallet.addExceptionObserver(#selector(tappayEasyWalletExceptionHandler(notofication:)))
         return true
     }
     
-    public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        let tapPayHandled = TPDLinePay.handle(url)
-        if (tapPayHandled) {
-            return true
-        }
-        
-        return false
+    public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        TPDLinePay.handle(url)
     }
     
     public func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]) -> Void) -> Bool {
@@ -37,87 +70,88 @@ public class SwiftTappayflutterpluginPlugin: NSObject, FlutterPlugin {
         return true
     }
     
-    @objc func tappayLinePayExceptionHandler(notofication: Notification) {
-        
-        let result : TPDLinePayResult = TPDLinePay.parseURL(notofication)
-        
-        print("status : \(result.status) , orderNumber : \(result.orderNumber ?? "") , recTradeid : \(result.recTradeId ?? "") , bankTransactionId : \(result.bankTransactionId ?? "") ")
-        
+    @objc private func tappayLinePayExceptionHandler(notofication: Notification) {
+        let result = TPDLinePay.parseURL(notofication)
+        debugPrint("status: \(result.status)")
+        debugPrint("orderNumber: \(result.orderNumber ?? "")")
+        debugPrint("recTradeid: \(result.recTradeId ?? "")")
+        debugPrint("bankTransactionId: \(result.bankTransactionId ?? "")")
     }
     
-    @objc func tappayEasyWalletExceptionHandler(notofication: Notification) {
-        
-        let result : TPDEasyWalletResult = TPDEasyWallet.parseURL(notofication)
-        
-        print("status : \(result.status) , orderNumber : \(result.orderNumber ?? "") , recTradeid : \(result.recTradeId ?? "") , bankTransactionId : \(result.bankTransactionId ?? "") ")
-        
+    @objc private func tappayEasyWalletExceptionHandler(notofication: Notification) {
+        guard let result = TPDEasyWallet.parseURL(notofication) else {
+            debugPrint("TPDEasyWalletResult is nil")
+            return
+        }
+        debugPrint("status: \(result.status)")
+        debugPrint("orderNumber: \(result.orderNumber ?? "")")
+        debugPrint("recTradeid: \(result.recTradeId ?? "")")
+        debugPrint("bankTransactionId: \(result.bankTransactionId ?? "")")
     }
-    
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        
-        let method = call.method
-        
+
+        let method = Call(call.method) ?? .undefined
         guard let args = call.arguments as? [String:Any] else {
             result("args cast error")
             return
         }
-        
+
         switch method {
-        case "setupTappay":
+        case .setupTappay:
             setupTappay(args: args) { (error) in
                 result(error)
             }
             
-        case "isCardValid":
+        case .isCardValid:
             result(isCardValid(args: args))
             
-        case "getPrime":
+        case .getPrime:
             getPrime(args: args) { (prime) in
                 result(prime)
             } failCallBack: { (message) in
                 result(message)
             }
             
-        case "isEasyWalletAvailable":
+        case .isEasyWalletAvailable:
             result(isEasyWalletAvailable())
             
-        case "getEasyWalletPrime":
+        case .getEasyWalletPrime:
             getEasyWalletPrime(args: args) { (prime) in
                 result(prime)
             } failCallBack: { (message) in
                 result(message)
             }
             
-        case "redirectToEasyWallet":
+        case .redirectToEasyWallet:
             redirectToEasyWallet(args: args) { (callBack) in
                 result(callBack)
             }
             
-        case "isLinePayAvailable":
+        case .isLinePayAvailable:
             result(isLinePayAvailable())
             
-        case "getLinePayPrime":
+        case .getLinePayPrime:
             getLinePayPrime(args: args) { (prime) in
                 result(prime)
             } failCallBack: { (message) in
                 result(message)
             }
-            
-        case "redirectToLinePay":
+
+        case .redirectToLinePay:
             redirectToLinePay(args: args) { (callBack) in
                 result(callBack)
             }
+
         default:
             result("iOS " + UIDevice.current.systemVersion)
         }
-        
     }
     
-    //設置Tappay環境
-    fileprivate func setupTappay(args: [String:Any], errorMessage: @escaping(String) -> Void) {
+    /// 設置 `Tappay` 環境
+    private func setupTappay(args: [String: Any], errorMessage: @escaping(String) -> Void) {
         
-        var message: String = ""
+        var message = ""
         
         let appId = (args["appId"] as? Int32 ?? 0)
         let appKey = (args["appKey"] as? String ?? "")
@@ -139,37 +173,35 @@ public class SwiftTappayflutterpluginPlugin: NSObject, FlutterPlugin {
             errorMessage(message)
             return
         }
-        
-        let st: TPDServerType = {
-            return serverType == "sandBox" ? TPDServerType.sandBox : TPDServerType.production
-        }()
-        
-        
-        TPDSetup.setWithAppId(appId, withAppKey: appKey, with: st)
+
+        let type = serverType == "sandBox" ? TPDServerType.sandBox : TPDServerType.production
+
+        TPDSetup.setWithAppId(appId, withAppKey: appKey, with: type)
 //        TPDSetup.shareInstance().setupIDFA(ASIdentifierManager.shared().advertisingIdentifier.uuidString)
 //        TPDSetup.shareInstance().serverSync()
     }
     
-    //檢查信用卡的有效性
-    fileprivate func isCardValid(args: [String:Any]) -> Bool {
+    /// 檢查信用卡的有效性
+    private func isCardValid(args: [String:Any]) -> Bool {
         
         let cardNumber = (args["cardNumber"] as? String ?? "")
         let dueMonth = (args["dueMonth"] as? String ?? "")
         let dueYear = (args["dueYear"] as? String ?? "")
         let ccv = (args["ccv"] as? String ?? "")
-        
-        
-        guard let cardValidResult = TPDCard.validate(withCardNumber: cardNumber, withDueMonth: dueMonth, withDueYear: dueYear, withCCV: ccv) else { return false }
-        
-        if cardValidResult.isCardNumberValid && cardValidResult.isExpiryDateValid && cardValidResult.isCCVValid {
-            return true
-        }else{
+
+        guard let cardValidResult = TPDCard.validate(
+            withCardNumber: cardNumber,
+            withDueMonth: dueMonth,
+            withDueYear: dueYear,
+            withCCV: ccv)
+        else {
             return false
         }
+        return cardValidResult.isCardNumberValid && cardValidResult.isExpiryDateValid && cardValidResult.isCCVValid
     }
     
-    //取得prime
-    fileprivate func getPrime(args: [String:Any], prime: @escaping(String) -> Void, failCallBack: @escaping(String) -> Void) {
+    /// 取得 `Prime`
+    private func getPrime(args: [String: Any], prime: @escaping(String) -> Void, failCallBack: @escaping(String) -> Void) {
         
         let cardNumber = (args["cardNumber"] as? String ?? "")
         let dueMonth = (args["dueMonth"] as? String ?? "")
@@ -177,23 +209,25 @@ public class SwiftTappayflutterpluginPlugin: NSObject, FlutterPlugin {
         let ccv = (args["ccv"] as? String ?? "")
         
         let card = TPDCard.setWithCardNumber(cardNumber, withDueMonth: dueMonth, withDueYear: dueYear, withCCV: ccv)
-        card.onSuccessCallback { (tpPrime, cardInfo, cardIdentifier, merchantReferenceInfo) in
-            if let tpPrime = tpPrime {
-                prime("{\"status\":\"\", \"message\":\"\", \"prime\":\"\(tpPrime)\"}")
+        card
+            .onSuccessCallback { (tpPrime, cardInfo, cardIdentifier, merchantReferenceInfo) in
+                if let tpPrime = tpPrime {
+                    prime("{\"status\":\"\", \"message\":\"\", \"prime\":\"\(tpPrime)\"}")
+                }
             }
-        }.onFailureCallback { (status, message) in
-            failCallBack("{\"status\":\"\(status)\", \"message\":\"\(message)\", \"prime\":\"\"}")
-        }.createToken(withGeoLocation: "UNKNOWN")
+            .onFailureCallback { (status, message) in
+                failCallBack("{\"status\":\"\(status)\", \"message\":\"\(message)\", \"prime\":\"\"}")
+            }
+            .createToken(withGeoLocation: "UNKNOWN")
     }
     
-    //檢查是否有安裝Easy wallet
-    fileprivate func isEasyWalletAvailable() -> Bool {
-        return TPDEasyWallet.isEasyWalletAvailable()
+    /// 檢查是否有安裝 Easy wallet
+    private func isEasyWalletAvailable() -> Bool {
+        TPDEasyWallet.isEasyWalletAvailable()
     }
     
-    
-    //取得Easy wallet prime
-    fileprivate func getEasyWalletPrime(args: [String:Any], prime: @escaping(String) -> Void, failCallBack: @escaping(String) -> Void) {
+    /// 取得Easy wallet prime
+    private func getEasyWalletPrime(args: [String:Any], prime: @escaping(String) -> Void, failCallBack: @escaping(String) -> Void) {
         
         let universalLink = (args["universalLink"] as? String ?? "")
         
@@ -217,27 +251,26 @@ public class SwiftTappayflutterpluginPlugin: NSObject, FlutterPlugin {
         
     }
     
-    //重導向至Easy wallet
-    fileprivate func redirectToEasyWallet(args: [String:Any], callBack: @escaping(String) -> Void) {
-        
+    /// 重導向至Easy wallet
+    private func redirectToEasyWallet(args: [String:Any], callBack: @escaping(String) -> Void) {
+
         let universalLink = (args["universalLink"] as? String ?? "")
         let easyWallet = TPDEasyWallet.setup(withReturUrl: universalLink)
         
         let paymentUrl = (args["paymentUrl"] as? String ?? "")
-        easyWallet.redirect(paymentUrl) { (result) in
+        easyWallet.redirect(paymentUrl) { result in
             callBack("{\"status\":\"\(String(result.status))\", \"recTradeId\":\"\(String(result.recTradeId))\", \"orderNumber\":\"\(String(result.orderNumber))\", \"bankTransactionId\":\"\(String(result.bankTransactionId))\"}")
         }
     }
     
-    //檢查是否有安裝Line pay
-    fileprivate func isLinePayAvailable() -> Bool {
-        let result = TPDLinePay.isLinePayAvailable()
-        return result
+    /// 檢查是否有安裝Line pay
+    private func isLinePayAvailable() -> Bool {
+        TPDLinePay.isLinePayAvailable()
     }
     
     
-    //取得line pay prime
-    fileprivate func getLinePayPrime(args: [String:Any], prime: @escaping(String) -> Void, failCallBack: @escaping(String) -> Void) {
+    /// 取得line pay prime
+    private func getLinePayPrime(args: [String:Any], prime: @escaping(String) -> Void, failCallBack: @escaping(String) -> Void) {
         
         let universalLink = (args["universalLink"] as? String ?? "")
         
@@ -247,35 +280,35 @@ public class SwiftTappayflutterpluginPlugin: NSObject, FlutterPlugin {
         }
         
         let linePay = TPDLinePay.setup(withReturnUrl: universalLink)
-        linePay.onSuccessCallback { (tpPrime) in
-            
-            if let tpPrime = tpPrime {
-                prime("{\"status\":\"\", \"message\":\"\", \"prime\":\"\(tpPrime)\"}")
+        linePay
+            .onSuccessCallback { (tpPrime) in
+                if let tpPrime = tpPrime {
+                    prime("{\"status\":\"\", \"message\":\"\", \"prime\":\"\(tpPrime)\"}")
+                }
             }
-            
-        }.onFailureCallback { (status, message) in
-            
-            failCallBack("{\"status\":\"\(status)\", \"message\":\"\(message)\", \"prime\":\"\"}")
-            
-        }.getPrime()
+            .onFailureCallback { (status, message) in
+                failCallBack("{\"status\":\"\(status)\", \"message\":\"\(message)\", \"prime\":\"\"}")
+
+            }
+            .getPrime()
         
     }
     
-    //重導向至line pay
-    fileprivate func redirectToLinePay(args: [String:Any], callBack: @escaping(String) -> Void) {
-        
+    /// 重導向至 Line Pay
+    private func redirectToLinePay(args: [String:Any], callBack: @escaping(String) -> Void) {
         let universalLink = (args["universalLink"] as? String ?? "")
         let linePay = TPDLinePay.setup(withReturnUrl: universalLink)
-        
         let paymentUrl = (args["paymentUrl"] as? String ?? "")
         
 //        let rootViewController = UIApplication.shared.windows.filter({ (w) -> Bool in
 //                    return w.isHidden == false
 //         }).first?.rootViewController
         
-        guard let vc = UIApplication.shared.delegate?.window??.rootViewController else { return }
+        guard let vc = UIApplication.shared.delegate?.window??.rootViewController else {
+            return
+        }
         
-        linePay.redirect(paymentUrl, with: vc) { (result) in
+        linePay.redirect(paymentUrl, with: vc) { result in
             callBack("{\"status\":\"\(String(result.status))\", \"recTradeId\":\"\(String(result.recTradeId))\", \"orderNumber\":\"\(String(result.orderNumber))\", \"bankTransactionId\":\"\(String(result.bankTransactionId))\"}")
         }
     }
